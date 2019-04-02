0ed1edf9b3dfab2c2620d52503e1accf84dd2564
lskinner2350 <48497111+lskinner2350@users.noreply.github.com>
2019-04-03 10:28:30 +1100

    Caeser Cypher basic

--- Main.c
+++ Main.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
+#include <math.h>
+void encrypt(char *x, int N, int k);
 
 int main()
+{
+    char enc[] = "TESTDECRYPTION";
+    int k = 2;
+    encrypt(enc, 14, k);
+    printf("Decryption is: %s", enc);
+    return 0;
+
+}
+
+
+
+
+void encrypt(char *x, int N, int k)
+{
+int i = 0;
+for(i = 0; i < N; i++)
+{
+    printf("%c\t", x[i]);
+    x[i] = ((x[i] + k)%26 + 65 );
+    printf("%c\n", x[i]);
+}
+return;
+}
