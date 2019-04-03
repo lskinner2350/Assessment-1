19a74328c9a85570441b0e091e7614e27dad04c2
lskinner2350 <48497111+lskinner2350@users.noreply.github.com>
2019-04-03 11:37:57 +1100

    Update Main.c

--- Main.c
+++ Main.c
@@ -10,7 +10,7 @@ int main()
     char enc[20];
     printf("Code to be Decrypted: ");
     scanf("%s", enc);
-    int k = 3;
+    int k = 15;
     int c;
     encrypt(enc, k);
     printf("Encryption is: %s", enc);
@@ -24,14 +24,14 @@ int main()
 
 void encrypt(char *x, int k)
 {
-    int N = sizeof(x);
     int i = 0;
-    for(i = 0; i < N + 1; i++)
+    do
     {
         printf("%c\t", x[i]);
         x[i] = ((x[i] + k)%26 + 65 );
         printf("%c\n", x[i]);
-    }
+        i++;
+    } while(x[i] != NULL);
     return;
 }
 
@@ -41,12 +41,13 @@ void encrypt(char *x, int k)
 void decrypt(char *x, int N, int k)
 {
     int i = 0;
-    for(i = 0; i < N; i++)
+    do
     {
         printf("%c\t", x[i]);
         x[i] = ((x[i] - k)%26 + 65 );
         printf("%c\n", x[i]);
-    }
+        i++;
+    } while(x[i] != NULL);
     return;
 }
 void brutedecrypt(char *x, int N, int k)
